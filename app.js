155ff5830d31c39018b09ea12153225163fc9aa3
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-14 00:47:05 +0600

    app.js updated

--- app.js
+++ app.js
@@ -43,8 +43,7 @@ app.post("/",function(req, res){
     const url = "https://us8.api.mailchimp.com/3.0/lists/bdd0e0631f"
 
     const options = {
-        method: "POST",
-        auth: "Sadmin:ac0c02988c9952b603583212ab670d628-us8"
+        method: "POST"
     }
 
     const request = https.request(url, options, function(response){
@@ -67,9 +66,3 @@ app.post("/",function(req, res){
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server set at port 3000");
 });
-
-// API key
-// c0c02988c9952b603583212ab670d628-us8
-
-// Audience ID
-// bdd0e0631f
