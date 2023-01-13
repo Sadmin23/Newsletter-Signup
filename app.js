e6289a47aa0ba333249d1c4ca8be6d609be57951
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-13 18:32:13 +0600

    success page updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // const request = require("request");
 const https = require("https");
+const { request } = require("http");
 
 const app = express();
 
@@ -12,15 +13,15 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 })
 
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
 app.post("/",function(req, res){
 
     const firstName = req.body.First_Name;
     const lastName = req.body.Last_Name;
     const email = req.body.Email;
-
-    // console.log(req.body.First_Name);
-    // console.log(req.body.Last_Name);
-    // console.log(req.body.Email);
 
     var data = {
         members: 
@@ -29,18 +30,38 @@ app.post("/",function(req, res){
                 email_address: email,
                 status: "subscribed",
                 merge_fields:
-                [
-                    {
-                        FNAME: firstName,
-                        LNAME: lastName
-                    }
-                ]  
+                {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
             }
         ]
     };
 
     const jsonData = JSON.stringify(data);
 
+    const url = "https://us8.api.mailchimp.com/3.0/lists/bdd0e0631f"
+
+    const options = {
+        method: "POST",
+        auth: "Sadmin:ac0c02988c9952b603583212ab670d628-us8"
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode === 200){
+            res.sendFile(__dirname+"/success.html");
+        } else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
 })
 
 app.listen(3000, function(){
